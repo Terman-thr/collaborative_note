08a60d9e118858b2b1d1fea98a6141196727359b
HaoranTang <thr@umich.edu>
2023-02-16 11:39:49 -0500

    send back message

--- chat/consumers.py
+++ chat/consumers.py
@@ -10,3 +10,14 @@ class ChatConsumer(WebsocketConsumer):
             'type': 'connection_established',
             'message': 'You are now connected!'
         }))
+
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        print('Message:',message)
+
+        self.send(text_data=json.dumps({
+            'type': 'chat',
+            'message': message
+        }))
