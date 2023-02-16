9291ea6cc4dcf04e99eed76b7ee3137d48a3fbe4
HaoranTang <thr@umich.edu>
2023-02-16 12:42:48 -0500

    send article to server and groups

--- chat/consumers.py
+++ chat/consumers.py
@@ -18,17 +18,45 @@ class ChatConsumer(WebsocketConsumer):
             'message': 'You are now connected!'
         }))
 
+
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+        type = text_data_json['type']
+        if type == 'highlight':
+            article = text_data_json['article']
+            highlightCnt = text_data_json['highlightCnt']
+            print("article:", article)
+            print("highlightCnt:", highlightCnt)
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_group_name,
+                {
+                    'type': 'article_text',
+                    'article': article,
+                    'highlightCnt': highlightCnt
+                }
+            )
+            
+        elif type == 'chat':
+            message = text_data_json['message']
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_group_name,
+                {
+                    'type': 'chat_message',
+                    'message': message
+                }
+            )
 
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message
-            }
-        )
+
+    def article_text(self, event):
+        article = event['article']
+        highlightCnt = event['highlightCnt']
+        self.send(text_data=json.dumps({
+            'type': 'article',
+            'article': article,
+            'highlightCnt': highlightCnt
+        }))
+
+
 
     def chat_message(self, event):
         message = event['message']
